42303a866b18c54e37513ab1a1c09879a93be047
Priyobrato Das <priyodas12@gmail.com>
2022-09-10 17:54:31 +0530

    OPP 3

--- OOP/Sample.py
+++ OOP/Sample.py
@@ -1,12 +1,13 @@
 class Dog():
-    def __init__(self, breed, age, cityOrigin):
+    def __init__(self, breed, age, cityOrigin, isMale):
         self.breed = breed
         self.age = age
+        self.isMale = isMale
         self.cOrigin = cityOrigin
         # self.attribute_name is actual class property
         # arguments in init can difer
 
 
-dog = Dog("pomerian", 12, "BLR")
+dog = Dog("pomerian", 14, "KOL", True)
 
-print(dog.age, dog.breed, dog.cOrigin)
+print(dog.age, dog.breed, dog.cOrigin, dog.isMale)
