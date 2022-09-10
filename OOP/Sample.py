ec553701decccc2ac452c31dbef3cb80ba9c282e
Priyobrato Das <priyodas12@gmail.com>
2022-09-10 17:44:57 +0530

    OOP2
    Signed-off-by: Priyobrato Das <priyodas12@gmail.com>

--- OOP/Sample.py
+++ OOP/Sample.py
@@ -1,9 +1,12 @@
 class Dog():
-    def __init__(self, breed, age):
+    def __init__(self, breed, age, cityOrigin):
         self.breed = breed
         self.age = age
+        self.cOrigin = cityOrigin
+        # self.attribute_name is actual class property
+        # arguments in init can difer
 
 
-dog = Dog("pomerian", 12)
+dog = Dog("pomerian", 12, "BLR")
 
-print(dog.age, dog.breed)
+print(dog.age, dog.breed, dog.cOrigin)
