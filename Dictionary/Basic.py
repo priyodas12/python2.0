10e387c389dc4a786b2c98a17561d4db95e88128
Priyobrato Das <priyodas12@gmail.com>
2022-09-23 09:30:37 +0530

    dictionary basics

---
+++ Dictionary/Basic.py
@@ -0,0 +1,15 @@
+tel = {'jack': 4098, 'sape': 4139, 'trie': 9273}
+
+# print whole dic
+print(tel)
+
+# print key based value
+print(tel['jack'])
+
+# delete any key
+del (tel['trie'])
+print(tel)
+
+# insert elements
+tel.update({'myer': 2538})
+print(tel)
---
+++ Dictionary/Iteration.py
@@ -0,0 +1,5 @@
+score_card = {'mike': 334, 'jill': 462, 'keyra': 342}
+
+# print keys and values
+print(list(score_card.keys()))
+print(list(score_card.values()))
