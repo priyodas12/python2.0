d4982e2da420f46bbacdda041812fa3d26ad5783
Priyobrato Das <priyodas12@gmail.com>
2022-09-23 08:08:42 +0530

    list api 1

---
+++ ListOps/ListAddApi.py
@@ -0,0 +1,15 @@
+friends = ["alex", "bob", "julia", "max", "roy"]
+
+# access all elements
+print(friends)
+# access single elements
+print(friends[1])
+# access elemnts from back,last elemnts starts from [-1]
+print(friends[-2])
+
+# selection of elemnts from specific index
+print(friends[1:])  # from index[1] to rest
+# from index[1] to index[3] where index[3] will be exclusive,list[inclusive:exclusive]
+print(friends[1:3])
+
+print(friends[1:3:2])
