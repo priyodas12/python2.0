ecf5d2910918a245a9dfec6942c68cc5550d902c
Priyobrato Das <priyodas12@gmail.com>
2022-09-23 09:36:50 +0530

    dictionary

--- Dictionary/Iteration.py
+++ Dictionary/Iteration.py
@@ -3,3 +3,6 @@ score_card = {'mike': 334, 'jill': 462, 'keyra': 342}
 # print keys and values
 print(list(score_card.keys()))
 print(list(score_card.values()))
+
+for key, value in score_card.items():
+    print(f" {key} -> {value}")
