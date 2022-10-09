7afd33e31f1127a17105f167463057b347ee8a91
priyo das <priyodas12@gmail.com>
2022-10-09 20:55:38 +0530

    set ops

---
+++ Sets/BasicSets.py
@@ -0,0 +1,8 @@
+# unordered, mutable, no duplicates
+numbers_1 = {1, 2, 3, 4, 4}
+
+print(numbers_1)
+
+numbers_2 = set(list(range(1, 20)))
+
+print(numbers_2)
---
+++ Sets/SetOps.py
@@ -0,0 +1,16 @@
+nums = set()
+
+# add elements
+nums.add(1)
+nums.add(4)
+nums.add(3)
+nums.add(2)
+nums.add(1)
+
+print(nums)
+
+# remove elements
+nums.remove(3)
+print(nums)
+nums.discard(2)
+print(nums)
